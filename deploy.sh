c091b94c6747e13ab82a073de9e486e7ac4b8914
Weblime <weblime@Weblimes-MacBook-Pro.local>
2020-05-17 14:47:45 +0300

    some deployment polish

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-echo 'Going deployment'
+echo 'Going deployment API'
 ssh root@weblime.ru "cd ~/de/production/pasqooda/pasqooda-api; git pull origin master; git status; docker-compose build; docker-compose up"
+echo 'Going deployment FRONT'
+ssh root@weblime.ru "cd ~/de/production/pasqooda/pasqooda-front; git pull origin master; git status; docker-compose build; docker-compose up"
